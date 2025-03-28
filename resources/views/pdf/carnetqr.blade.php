1dc3486cc662ab222c869f39667aa05859ffdc93
kepfex <kevinlepoma@gmail.com>
2025-03-28 16:36:42 -0500

    add normalize y estilos enlazados para Dompdf

--- resources/views/pdf/carnetqr.blade.php
+++ resources/views/pdf/carnetqr.blade.php
@@ -10,44 +10,30 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Carnets QR - Estudiantes</title>
+
+    <link rel="stylesheet" href="{{ public_path('css/normalize.css') }}">
+    <link rel="stylesheet" href="{{ public_path('css/pdf/carnet-qr.css') }}">
 </head>
 
 <body>
     <h2>Lista de Carnets Generados</h2>
 
     <table>
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Alumno</th>
-                <th>Grado</th>
-                <th>Sección</th>
-            </tr>
-        </thead>
-        <tbody>
+        <tr>
             @foreach ($matriculas as $index => $matricula)
-            <tr>
-                <td>{{ $index + 1 }}</td>
-                <td>{{ $matricula->alumno->nombres }} {{ $matricula->alumno->apellidos }}</td>
-                <td>{{ $matricula->grado->nombre }}</td>
-                <td>{{ $matricula->seccion->nombre }}</td>
-            </tr>
+                @if ($index % 2 == 0) </tr><tr> @endif
+                <td>
+                    <div class="card">
+                        <h2>{{ $matricula->alumno->nombres }} {{ $matricula->alumno->apellidos }}</h2>
+                        <p>Grado: {{ $matricula->grado->nombre }}</p>
+                        <p>Sección: {{ $matricula->seccion->nombre }}</p>
+                    </div>
+                </td>
             @endforeach
-        </tbody>
+        </tr>
     </table>
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
--->
 </body>
 
 </html>
